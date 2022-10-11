2621f000129f59fc39b7181e8b1aabbe9d6bca5a
MarcoGomezGutierrez <81591332+MarcoGomezGutierrez@users.noreply.github.com>
2022-10-11 17:23:15 +0000

    Prueba de clase con Node.js y borrar carpeta de Gatsby

--- JavaScript/HelloWorld/helloworld.js
+++ JavaScript/HelloWorld/helloworld.js
@@ -2,3 +2,16 @@ console.log("Hellor World");
 let palabra = "marco";
 console.log(`hola ${palabra}`)
 console.log("Hola " + palabra);
+
+let obj = {
+    "8":5,
+    "3":2,
+    "1":3,
+    nombre:1
+}
+
+console.log(obj);
+
+delete obj.nombre;
+
+console.log(obj.nombre);
